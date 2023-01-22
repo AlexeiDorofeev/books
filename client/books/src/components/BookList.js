b5b33faf340d82eb76767db0184c3c09d5de023f
Alexei.Dorofeev <Alexei.Dorofeev@wolterskluwer.com>
2023-01-22 18:31:57 +0100

    client configs

---
+++ client/books/src/components/BookList.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+function BooksList() {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/books')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setBooks(data);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {books.map(book => (
+                        <li key={book.id}>
+                            {book.name} by {book.author}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default BooksList;
